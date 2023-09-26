c9fde9ba275e8cd59b643da5399709a0bb43af5d
MasuetASF <128169979+MasuetASF@users.noreply.github.com>
2023-09-26 19:30:27 -0300

    Modulo Finalizado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,12 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { MenuComponent} from './components/menu.component';
-import {Button} from './components/button.component'
+import {Button} from './components/button.component';
+import {EntryDataComponent} from './components/entry-data.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, MenuComponent, Button ],
+  declarations: [ AppComponent, HelloComponent, MenuComponent, Button, EntryDataComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }

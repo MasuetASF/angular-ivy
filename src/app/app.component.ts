b2d5e7dc556f4502fe2c46d3f0f5c66d11399b46
MasuetASF <128169979+MasuetASF@users.noreply.github.com>
2023-09-26 16:54:22 -0300

    Criando Components reutilizáveis.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component, VERSION } from '@angular/core';
 
 @Component({
-  selector: 'my-app',
+  selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
   name = 'Angular ' + VERSION.major;
+  buttonLabel:string ='CARRINHO';
+  buttonSecond:string = 'ADICIONAR A FAVORITOS'
 }
---
+++ src/app/components/menu.component.ts
@@ -0,0 +1,13 @@
+import {Component} from '@angular/core';
+ 
+
+
+@Component({
+  selector: 'menu-component', 
+  template: '<h2> Mateus</h2>',
+  styles: [`h2{color: green}`],
+})
+
+export class MenuComponent{
+
+}
